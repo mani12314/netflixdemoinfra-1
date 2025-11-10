88ecf7c1effaad607543f6c49e15830e04d656ba
mani12314 <manikantapalakodeti198@gmail.com>
2025-11-10 17:45:32 +0530

    Update AMI, key name, and security group for instance

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ region = "ap-southeast-2"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0d53a60306ca7e710"
+ami = "ami-03c7b1a0c7cc99160"
 instance_type = "t2.micro"
-key_name = "jekins"
-vpc_security_group_ids = ["sg-080a6d0011f38981d"]
+key_name = "poy"
+vpc_security_group_ids = ["sg-0ace683866cedd2f2"]
 tags = {
 Name = var.instance_names[count.index]
 }
