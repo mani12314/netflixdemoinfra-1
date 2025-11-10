897cc4fe219ca05f4798fe53eeb6c6a5faad075d
mani12314 <manikantapalakodeti198@gmail.com>
2025-11-10 17:00:04 +0530

    Update AWS region and instance configuration

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 
 provider "aws" {
-region = "us-east-1"
+region = "ap-southeast-2"
 }
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-051e483428ae60e7d"
+ami = "ami-0d53a60306ca7e710"
 instance_type = "t2.micro"
-key_name = "mykey"
-vpc_security_group_ids = ["sg-0cec4f77ba86c6536"]
+key_name = "jekins"
+vpc_security_group_ids = ["sg-080a6d0011f38981d"]
 tags = {
 Name = var.instance_names[count.index]
 }
