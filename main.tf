233eafa3b8b94b3c9bbd888901f96fc2c137b28a
mani12314 <manikantapalakodeti198@gmail.com>
2025-11-10 16:19:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
+
 provider "aws" {
-  region = "us-east-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  count                  = 4
-  ami                    = "ami-051e483428ae60e7d"
-  instance_type          = "t2.micro"
-  key_name               = "netflix"
-  vpc_security_group_ids = ["sg-0cec4f77ba86c6536"]
-  tags = {
-    Name = var.instance_names[count.index]
-  }
+count = 4
+ami = "ami-051e483428ae60e7d"
+instance_type = "t2.micro"
+key_name = "mykey"
+vpc_security_group_ids = ["sg-0cec4f77ba86c6536"]
+tags = {
+Name = var.instance_names[count.index]
+}
 }
 
 variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+default = ["jenkins", "APPSERVER-1", "APPSERVER-2", "Monitoring server"]
 }
