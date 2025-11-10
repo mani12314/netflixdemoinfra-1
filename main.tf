fc50e57bc8dcca33a202d2ff1ab496c7a6c51def
mani12314 <manikantapalakodeti198@gmail.com>
2025-11-10 13:34:19 +0530

    Update AMI and security group for AWS instance

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-0b8d527345fdace59"
+  ami                    = "ami-051e483428ae60e7d"
   instance_type          = "t2.micro"
   key_name               = "netflix"
-  vpc_security_group_ids = ["sg-0de2cffd9b437ba98"]
+  vpc_security_group_ids = ["sg-0cec4f77ba86c6536"]
   tags = {
     Name = var.instance_names[count.index]
   }
